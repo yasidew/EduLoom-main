c3c57b9f8fba9c3f5e3231fc02907bd02eac7cee
IT21440236 <it21440236@my.sliit.lk>
2024-04-24 08:26:19 +0530

    EmailSenderService implementation

--- src/main/java/com/ds/edustack/notification/config/MailConfig.java
+++ src/main/java/com/ds/edustack/notification/config/MailConfig.java
@@ -16,8 +16,8 @@ public class MailConfig {
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
 
-        mailSender.setUsername("my.gmail@gmail.com");
-        mailSender.setPassword("password");
+        mailSender.setUsername("kodithuwakkusanu@gmail.com");
+        mailSender.setPassword("nvnztizfofnllaxb");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
